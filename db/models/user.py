55c5ef44ae49cfa1af2ac4f657b848b26b9e8b10
Eddince <126356013+Eddince@users.noreply.github.com>
2024-12-02 17:02:29 -0500

    user.py

--- db/models/user.py
+++ db/models/user.py
@@ -1,9 +1,10 @@
 
 from pydantic import BaseModel
-
+from typing import Optional
 
 class User(BaseModel):
-    id: str | None #El none hace que puede que el id no llegue, cuando se introduce un user
+    #id: str | None #El none hace que puede que el id no llegue, cuando se introduce un user
+    id: Optional[str]
     username: str
     email: str
     
